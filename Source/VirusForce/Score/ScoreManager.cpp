c914fb00786083bd57e1281a4369dc0cfd2c0abe
Adam <adam.a.farmer4129@gmail.com>
2020-04-10 18:59:21 -0700

    removes overall score multiplier

--- Source/VirusForce/Score/ScoreManager.cpp
+++ Source/VirusForce/Score/ScoreManager.cpp
@@ -3,6 +3,8 @@
 
 #include "ScoreManager.h"
 #include "../NPC/Virus.h"
+#include "../NPC/Viruses/StraightVirus.h"
+#include "../NPC/Viruses/TrackingVirus.h"
 
 // Sets default values for this component's properties
 UScoreManager::UScoreManager()
@@ -24,13 +26,33 @@ void UScoreManager::BeginPlay()
 
 int32 UScoreManager::IncreaseScore(AVirus* Virus, int32 MarkedVirusMultiplier)
 {
-	int32 BaseScore = FindScoreByVirusType(Virus->VirusType);
+	int32 BaseScore = FindScoreByVirusType(FindVirusType(Virus));
 	int32 AntibodyMultiplier = GetAntibodyMultiplier(Virus);
-	Score += (BaseScore * AntibodyMultiplier * Multiplier * MarkedVirusMultiplier);
+	Score += (BaseScore * AntibodyMultiplier * MarkedVirusMultiplier);
 
 	NumberOfVirusConsumed++;
-
 	return Score;
+}
+
+//helper function to determine what type of virus was consumed
+//as more virus types are added to the game they will need to be added here
+EVirusType UScoreManager::FindVirusType(AVirus* Virus)
+{
+	AStraightVirus* StraightVirus = Cast<AStraightVirus>(Virus);
+	ATrackingVirus* TrackingVirus = Cast<ATrackingVirus>(Virus);
+
+	if (StraightVirus != nullptr)
+	{
+		return StraightVirus->VirusType;
+	}
+	else if (TrackingVirus != nullptr)
+	{
+		return TrackingVirus->VirusType;
+	}
+	else
+	{
+		return Virus->VirusType;
+	}
 }
 
 int32 UScoreManager::FindScoreByVirusType(EVirusType VirusType)
@@ -53,56 +75,7 @@ int32 UScoreManager::IncreaseVirusConsumedCount()
 	return NumberOfVirusConsumed++;
 }
 
-//tracks how many viruses have been consumed since death and sets multiplier accordingly
-int32 UScoreManager::SetScoreMultiplier()
-{
-	if (NumberOfVirusConsumed >= ThresholdOne && NumberOfVirusConsumed < ThresholdTwo)
-	{
-		Multiplier = MultiplierTwo;
-	}
-	else if (NumberOfVirusConsumed >= ThresholdTwo && NumberOfVirusConsumed < ThresholdThree)
-	{
-		Multiplier = MultiplierThree;
-	}
-	else if (NumberOfVirusConsumed >= ThresholdThree && NumberOfVirusConsumed < ThresholdFour)
-	{
-		Multiplier = MultiplierFour;
-	}
-	else if (NumberOfVirusConsumed >= ThresholdFour && NumberOfVirusConsumed < ThresholdFive)
-	{
-		Multiplier = MultiplierFive;
-	}
-	else if (NumberOfVirusConsumed >= ThresholdFive && NumberOfVirusConsumed < ThresholdSix)
-	{
-		Multiplier = MultiplierSix;
-	}
-	else if (NumberOfVirusConsumed >= ThresholdSix && NumberOfVirusConsumed < ThresholdSeven)
-	{
-		Multiplier = MultiplierSeven;
-	}
-	else if (NumberOfVirusConsumed >= ThresholdEight && NumberOfVirusConsumed < ThresholdNine)
-	{
-		Multiplier = MultiplierEight;
-	}
-	else if (NumberOfVirusConsumed >= ThresholdNine && NumberOfVirusConsumed < ThresholdTen)
-	{
-		Multiplier = MultiplierNine;
-	}
-	else if (NumberOfVirusConsumed >= ThresholdTen)
-	{
-		Multiplier = MultiplierTen;
-	}
-	else
-	{
-		Multiplier = MultiplierOne;
-	}
-	return Multiplier;
-}
-
 int32 UScoreManager::GetAntibodyMultiplier(AVirus* Virus)
 {
-	//TODO fix this right now will return 0 as it is just subtracting
-	// the number of attached slots from the max number of slots possible
-	// fix this in the virus class
 	return Virus->NumOfAttachedAntibodies();
 }
--- Source/VirusForce/Score/ScoreManager.h
+++ Source/VirusForce/Score/ScoreManager.h
@@ -13,37 +13,12 @@ class VIRUSFORCE_API UScoreManager : public UActorComponent
 {
 	GENERATED_BODY()
 	
-	int32 Multiplier = 1;
 	int32 NumberOfVirusConsumed = 0;
 
 	//Scores for viruses
 	int32 BaseVirusScore = 100;
 	int32 StraightVirusScore = 300;
 	int32 TrackingVirusScore = 500;
-
-	//Score Multipliers
-	int32 MultiplierOne = 1;
-	int32 MultiplierTwo = 2;
-	int32 MultiplierThree = 3;
-	int32 MultiplierFour = 5;
-	int32 MultiplierFive = 10;
-	int32 MultiplierSix = 50;
-	int32 MultiplierSeven = 100;
-	int32 MultiplierEight = 300;
-	int32 MultiplierNine = 500;
-	int32 MultiplierTen = 1000;
-
-	//Multiplier Thresholds
-	int32 ThresholdOne = 15;
-	int32 ThresholdTwo = 30;
-	int32 ThresholdThree = 60;
-	int32 ThresholdFour = 100;
-	int32 ThresholdFive = 150;
-	int32 ThresholdSix = 200;
-	int32 ThresholdSeven = 250;
-	int32 ThresholdEight = 300;
-	int32 ThresholdNine = 350;
-	int32 ThresholdTen = 400;
 
 public:	
 	// Sets default values for this component's properties
@@ -63,14 +38,14 @@ public:
 
 private:
 
+	//finds virus type of consumed virus
+	EVirusType FindVirusType(AVirus* Virus);
+
 	//finds score for virus type
 	int32 FindScoreByVirusType(EVirusType VirusType);
 
 	//tracks viruses consumed
 	int32 IncreaseVirusConsumedCount();
-
-	//sets overall score multiplier based on number of viruses consumed in one life
-	int32 SetScoreMultiplier();
 
 	//if the virus is fully marked give a bonus multiplier
 	int32 GetAntibodyMultiplier(AVirus* Virus);
