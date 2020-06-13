f5341f8f6746a5181c3271796c8c8c569dc1f36c
Adam <adam.a.farmer4129@gmail.com>
2020-06-12 18:00:30 -0700

    creates a burst virus class for new enemy type

---
+++ Source/VirusForce/NPC/Viruses/BurstVirus.h
@@ -0,0 +1,17 @@
+// Copyright Adam Farmer 2020
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../Virus.h"
+#include "BurstVirus.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIRUSFORCE_API ABurstVirus : public AVirus
+{
+	GENERATED_BODY()
+	
+};
