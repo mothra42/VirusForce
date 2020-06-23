916b6320fe99189c1b89692b25a161fc396c59fc
Adam <adam.a.farmer4129@gmail.com>
2020-06-23 16:42:17 -0700

    adds methods to burst virus to set infectable cell to pursue

--- Source/VirusForce/NPC/Viruses/BurstVirus.cpp
+++ Source/VirusForce/NPC/Viruses/BurstVirus.cpp
@@ -2,4 +2,51 @@
 
 
 #include "BurstVirus.h"
+#include "../NeutralCells/InfectableCell.h"
+#include "Kismet/GameplayStatics.h"
 
+void ABurstVirus::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Super::ShipMeshComponent->OnComponentHit.RemoveDynamic(this, &Super::OnHit);
+	Super::ShipMeshComponent->OnComponentHit.AddDynamic(this, &ABurstVirus::OnHit);
+	CheckWorldForInfectableCell();
+}
+
+void ABurstVirus::CheckWorldForInfectableCell()
+{
+	TArray<AActor*> FoundActors;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AInfectableCell::StaticClass(), FoundActors);
+
+	if (FoundActors.Num() >= 0)
+	{
+		SetInfectableCell(Cast<AInfectableCell>(FoundActors[0]));
+	}
+}
+
+void ABurstVirus::SetInfectableCell(AInfectableCell* InfectableCell)
+{
+	if (InfectableCell != nullptr)
+	{
+		CellToInfect = InfectableCell;
+	}
+}
+
+void ABurstVirus::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	//AInfectableCell* CellToInfect = Cast<AInfectableCell>(OtherActor);
+	if (Cast<AInfectableCell>(OtherActor) != nullptr)
+	{
+		InfectCell();
+	}
+
+	Super::OnHit(HitComp, OtherActor, OtherComp, NormalImpulse, Hit);
+}
+
+void ABurstVirus::InfectCell()
+{
+	CellToInfect->BeginInfection();
+	DestroyAttachedAntibodies();
+	Destroy();
+}
--- Source/VirusForce/NPC/Viruses/BurstVirus.h
+++ Source/VirusForce/NPC/Viruses/BurstVirus.h
@@ -14,4 +14,20 @@ class VIRUSFORCE_API ABurstVirus : public AVirus
 {
 	GENERATED_BODY()
 	
+	protected:
+		UPROPERTY(BlueprintReadOnly)
+		class AInfectableCell* CellToInfect;
+
+		virtual void BeginPlay() override;
+
+		virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) override;
+
+	private:
+		void InfectCell();
+
+		void CheckWorldForInfectableCell();
+
+	public:
+		void SetInfectableCell(AInfectableCell* InfectableCell);
 };
+
