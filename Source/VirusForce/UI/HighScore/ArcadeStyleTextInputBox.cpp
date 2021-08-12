f3f93c76295e5216fd1d1e4b8e3178d6cca63497
Adam <adam.a.farmer4129@gmail.com>
2021-08-11 17:47:24 -0700

    Adds New UI Class Based EditableTextBox
    This class is used to handle the logic for a scrolling letter input
    similar to those found in arcade leaderboard screens.

---
+++ Source/VirusForce/UI/HighScore/ArcadeStyleTextInputBox.cpp
@@ -0,0 +1,5 @@
+// Copyright Adam Farmer 2020
+
+
+#include "UI/HighScore/ArcadeStyleTextInputBox.h"
+
---
+++ Source/VirusForce/UI/HighScore/ArcadeStyleTextInputBox.h
@@ -0,0 +1,17 @@
+// Copyright Adam Farmer 2020
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/EditableTextBox.h"
+#include "ArcadeStyleTextInputBox.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIRUSFORCE_API UArcadeStyleTextInputBox : public UEditableTextBox
+{
+	GENERATED_BODY()
+	
+};
