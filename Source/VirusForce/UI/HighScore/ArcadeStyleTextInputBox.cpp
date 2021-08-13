b06c482978086ed0e82485ce960fa1dfdce11a86
Adam <adam.a.farmer4129@gmail.com>
2021-08-12 17:58:51 -0700

    Adds more functionality to ArcadeStyleInput
    Players can now cycle through letters of the alphabet with a gamepad,
    and the name is reflected in real time.

--- Source/VirusForce/UI/HighScore/ArcadeStyleTextInputBox.cpp
+++ Source/VirusForce/UI/HighScore/ArcadeStyleTextInputBox.cpp
@@ -1,5 +1,71 @@
 // Copyright Adam Farmer 2020
 
 
-#include "UI/HighScore/ArcadeStyleTextInputBox.h"
+#include "ArcadeStyleTextInputBox.h"
 
+UArcadeStyleTextInputBox::UArcadeStyleTextInputBox()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Constructor called"));
+	Text = FText::FromString(PlayerName);
+	ScrollRate = 0.3f;
+	ClearKeyboardFocusOnCommit = true;
+}
+
+FString UArcadeStyleTextInputBox::ScrollThroughLetters(bool IsUpInput)
+{
+	if (bCanScroll)
+	{
+		if (IsUpInput)
+		{
+			if (AlphabetIndex >= AlphabetArray.Num() - 1)
+			{
+				AlphabetIndex = 0;
+			}
+			else
+			{
+				AlphabetIndex++;
+			}
+		}
+		else
+		{
+			if (AlphabetIndex <= 0)
+			{
+				AlphabetIndex = AlphabetArray.Num() - 1;
+			}
+			else
+			{
+				AlphabetIndex--;
+			}
+		}
+		bCanScroll = false;
+		GetWorld()->GetTimerManager().SetTimer(TimerHandle_ScrollTimerExpired, this, &UArcadeStyleTextInputBox::ScrollTimerExpired, ScrollRate);
+		UE_LOG(LogTemp, Warning, TEXT("Current letter is %s"), *AlphabetArray[AlphabetIndex]);
+		SetText(FText::FromString(PlayerName + AlphabetArray[AlphabetIndex]));
+	}
+	return AlphabetArray[AlphabetIndex];
+}
+
+void UArcadeStyleTextInputBox::ConfirmLetter()
+{
+	if (AlphabetArray[AlphabetIndex] == "END")
+	{
+		HandleOnTextCommitted(FText::FromString(PlayerName), ETextCommit::OnEnter);
+		return;
+	}
+	if (PlayerName.Len() < NameCharLimit)
+	{
+		PlayerName += AlphabetArray[AlphabetIndex];
+		SetText(FText::FromString(PlayerName));
+	}
+}
+
+void UArcadeStyleTextInputBox::DeleteLetter()
+{
+	PlayerName.RemoveAt(PlayerName.Len() - 1);
+	SetText(FText::FromString(PlayerName));
+}
+
+void UArcadeStyleTextInputBox::ScrollTimerExpired()
+{
+	bCanScroll = true;
+}
--- Source/VirusForce/UI/HighScore/ArcadeStyleTextInputBox.h
+++ Source/VirusForce/UI/HighScore/ArcadeStyleTextInputBox.h
@@ -13,5 +13,38 @@ UCLASS()
 class VIRUSFORCE_API UArcadeStyleTextInputBox : public UEditableTextBox
 {
 	GENERATED_BODY()
-	
+
+public:
+	UArcadeStyleTextInputBox();
+
+	UFUNCTION(BlueprintCallable)
+	FString ScrollThroughLetters(bool IsUpInput);
+
+	UFUNCTION(BlueprintCallable)
+	void ConfirmLetter();
+
+	UFUNCTION(BlueprintCallable)
+	void DeleteLetter();
+
+	UPROPERTY(Category = UI, EditAnywhere, BlueprintReadWrite)
+	float ScrollRate;
+
+	void ScrollTimerExpired();
+
+	FORCEINLINE FString GetPlayerName() const { return PlayerName; }
+
+private:
+	TArray<FString> AlphabetArray = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+	"O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " END"};
+
+	int32 AlphabetIndex = 0;
+
+	bool bCanScroll = true;
+
+	FTimerHandle TimerHandle_ScrollTimerExpired;
+
+	FString PlayerName;
+
+	UPROPERTY(Category = NameLimits, EditAnywhere)
+	int32 NameCharLimit = 10;
 };
